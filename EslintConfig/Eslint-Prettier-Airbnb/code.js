6506c3535679b2fa097d842bf3a2f2d1cce9b775
Kshitiz <kshitiz.stha11@gmail.com>
2021-02-12 12:54:12 +0545

    folder structure commit first phase

---
+++ EslintConfig/Eslint-Prettier-Airbnb/code.js
@@ -0,0 +1,3 @@
+const items = {};
+// const item = {};
+console.log(items);
---
+++ hcsr with toolbar/src/pages/pageOne.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function pageOne() {
+  return (
+    <div id="homePageContainer">
+      <section id="heroSection">
+
+
+        
+      </section>
+    </div>
+  );
+}
